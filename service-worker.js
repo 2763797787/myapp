58f258e5a190a544c82585613bacbe95598222fa
2763797787 <2763797787@qq.com>
2025-12-19 12:37:18 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,68 @@
+// 缓存名称
+const CACHE_NAME = 'ej-risk-app-v1.0';
+
+// 需要缓存的资源列表
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // 添加其他资源文件（如果有的话）
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('已打开缓存');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活 Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('删除旧缓存:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有，返回缓存的内容
+        if (response) {
+          return response;
+        }
+        
+        // 否则从网络获取
+        return fetch(event.request).then(response => {
+          // 只缓存成功的响应
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // 克隆响应
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+            
+          return response;
+        });
+      })
+  );
+});
